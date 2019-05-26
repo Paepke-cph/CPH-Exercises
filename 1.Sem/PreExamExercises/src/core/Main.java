7d3a4f7fc1c306a034d3bab15e93863f50ef7b55
Benjamin Paepke <cph-bp133@cphbusiness.dk>
2019-05-26 20:47:06 +0200

    Final version?

--- 1.Sem/PreExamExercises/src/core/Main.java
+++ 1.Sem/PreExamExercises/src/core/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
 //        Grass grass = new Grass(new ConsoleUI());
 //        grass.run();
-
-        Squares squ = new Squares(new ConsoleUI());
-        squ.display(254, "#");
+//
+//        Squares squ = new Squares(new ConsoleUI());
+//        squ.display(254, "#");
     }
 }
